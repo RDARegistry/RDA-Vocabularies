31b5ed7fc723aa98e180143bc1420a7e061a13bd
GordonDunsire <RSCChair@rdatoolkit.org>
2020-12-14 14:45:27 +0000

    Update testelementScript.js

--- scripts/testelementScript.js
+++ scripts/testelementScript.js
@@ -425,7 +425,12 @@ if (typeof dataSource !== "undefined") {
                 "orderable": false,
                 "class": 'permalink',
                 "render": function (data, type, row) {
-                    return makeColumn(getLinkInLabel(getURI(row), " # "));
+//                    return makeColumn(getLinkInLabel(getURI(row), " # "));
+                    if (typeof row[ "@id"] != "undefined") {
+                        var url = makeURLFromURI(row[ "@id"]);
+                        var id = row[ "@id"].replace(/^.*\/(.*)$/ig, "$1");
+                        return '<a id="' + id + '" href="' + url + '" title="permalink: ' + url + '">#</a>';
+                    }
                 }
             }, {
                 "class": 'details-control',
