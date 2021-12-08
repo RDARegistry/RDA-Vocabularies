8e248747e1cacedfc43ad1430ffca625758f75ac
GordonDunsire <RSCChair@rdatoolkit.org>
2021-12-08 11:37:14 +0000

    Update testelementScript2.js

--- scripts/testelementScript2.js
+++ scripts/testelementScript2.js
@@ -192,7 +192,13 @@ if (typeof dataSource !== "undefined") {
     if (typeof uri != "undefined") {
       theURI = uri;
     }
-    if (theURI !== null && typeof theURI.replace === "function") {
+    if (theURI.indexOf("datatype") > 0) {
+      thePrefix += "d";
+    }
+     if (theURI.indexOf("object") > 0) {
+      thePrefix += "o";
+    }
+   if (theURI !== null && typeof theURI.replace === "function") {
       // replace everything up to last sub-folder slash with prefix and colon
       theCurie = thePrefix + ":" + theURI.substr(1 + theURI.lastIndexOf("/"));
     }
