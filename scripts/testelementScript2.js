4741c1ee280cd8e3be9c1c71d4ce5b00c0efb708
GordonDunsire <RSCChair@rdatoolkit.org>
2021-12-12 22:16:20 +0000

    test

--- scripts/testelementScript2.js
+++ scripts/testelementScript2.js
@@ -536,6 +536,10 @@ if (typeof dataSource !== "undefined") {
     $.getJSON(dataSource, function (json) {
       var theData;
       var theMetadata;
+      var theDownCSV = "";
+      var theDownJSON = "";
+      var theDownNT = "";
+      var theDownXML = "";
       var theVersionLink = "";
       var theVocDomain = "";
       var theVocTitle = "";
@@ -551,6 +555,7 @@ if (typeof dataSource !== "undefined") {
       theVocDomain = theVocTitle.replace(" properties", "");
       theVocToDatatype = '<a href="' + theVocURI + 'datatype/' + '">' +theVocTitle.replace("properties", "datatype properties") + '</a>';
       theVocToObject = '<a href="' + theVocURI + 'object/' + '">' +theVocTitle.replace("properties", "object properties") + '</a>';
+      theDownCSV = '<a href="http://www.rdaregistry.info/csv/Elements/' + window.curiePrefix + '.csv">CSV (text/csv)</a>';
       document.getElementById("vocTitle").innerHTML = theVocTitle;
       document.getElementById("vocDescription").innerHTML = theMetadata.description[ "en"];
       document.getElementById("vocURI").innerHTML = theVocURI;
