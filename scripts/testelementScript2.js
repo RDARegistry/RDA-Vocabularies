7efebca17fd19ee3f61610453e994eae5827d325
GordonDunsire <RSCChair@rdatoolkit.org>
2021-12-13 10:46:47 +0000

    Update testelementScript2.js

--- scripts/testelementScript2.js
+++ scripts/testelementScript2.js
@@ -489,7 +489,7 @@ if (typeof dataSource !== "undefined") {
   }
   
   function getPublished(value, index, array) {
-    return value["status"] = "Published";
+    return value["status"]["label"] = "Published";
   }
   function getLanguageCallout(data) {
     // not currently used: returns the xml language string
