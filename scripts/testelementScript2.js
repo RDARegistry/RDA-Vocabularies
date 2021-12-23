0075ac7c24cd74b69523091ce332559b0816fcfb
GordonDunsire <RSCChair@rdatoolkit.org>
2021-12-23 16:45:09 +0000

    Update testelementScript2.js

--- scripts/testelementScript2.js
+++ scripts/testelementScript2.js
@@ -818,8 +818,7 @@ if (typeof dataSource !== "undefined") {
     
     // Add event listener for opening and closing details
     
-    //    pageTable.children("tbody").on('click', 'td.details-control', function () {
-    table.children("tbody").on('click', 'td.details-control', function () {
+    pageTable.children("tbody").on('click', 'td.details-control', function () {
       // Get row containing the cell
       var tr = $(this).closest('tr');
       var row = table.row(tr);
@@ -828,11 +827,11 @@ if (typeof dataSource !== "undefined") {
         row.child.hide();
         //        tr.removeClass('shown');
       } else {
-        var cell = table.cell(this).node.html('<button class="btnCollapse" type="button"><i class="bi bi-arrows-collapse"> </i></button>');
+        var cell = table.cell(this).node;
         // Open this row
         row.child(formatDetail(row.data())).show();
         //        tr.addClass('shown');
-//        cell.html('<button class="btnCollapse" type="button"><i class="bi bi-arrows-collapse"> </i></button>');
+        cell.html('<button class="btnCollapse" type="button"><i class="bi bi-arrows-collapse"> </i></button>');
         table.page.draw('page');
       }
     });
