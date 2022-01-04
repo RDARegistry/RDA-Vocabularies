bd747fcecdcea31428aaf14bf1a393ebe39c1fd6
GordonDunsire <RSCChair@rdatoolkit.org>
2022-01-04 14:16:22 +0000

    test

--- Elements/test/localisation.js
+++ Elements/test/localisation.js
@@ -1,186 +1,26 @@
 {
   "en": {
-    "decimal": "",
-    "emptyTable": "No data available in table",
-    "info": "Showing _START_ to _END_ of _TOTAL_ entries",
-    "infoEmpty": "Showing 0 to 0 of 0 entries",
-    "infoFiltered": "(filtered from _MAX_ total entries)",
-    "thousands": ",",
-    "lengthMenu": "Show _MENU_ entries",
-    "loadingRecords": "Loading...",
-    "processing": "Processing...",
-    "search": "Search:",
-    "zeroRecords": "No matching records found",
-    "paginate": {
-      "first": "First",
-      "last": "Last",
-      "next": "Next",
-      "previous": "Previous"
-    },
-    "aria": {
-      "sortAscending": ": activate to sort column ascending",
-      "sortDescending": ": activate to sort column descending"
-    }
+    "search": "Search:"
   },
   "ar": {
-    "decimal": "",
-    "emptyTable": "No data available in table",
-    "info": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
-    "infoEmpty": "لا يوجد بيانات متاحة في الجدول",
-    "infoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
-    "thousands": ",",
-    "lengthMenu": "أظهر _MENU_ مدخلات",
-    "loadingRecords": "جارٍ التحميل...",
-    "processing": "جارٍ المعالجة...",
-    "search": "ابحث:",
-    "zeroRecords": "لم يعثر على أية سجلات",
-    "paginate": {
-      "first": "الأول",
-      "last": "الأخير",
-      "next": "التالي",
-      "previous": "السابق"
-    },
-    "aria": {
-      "sortAscending": ": تفعيل لترتيب العمود تصاعدياً",
-      "sortDescending": ": تفعيل لترتيب العمود تنازلياً"
-    }
+    "search": "ابحث:"
   },
   "ca": {
-    "decimal": ",",
-    "emptyTable": "No hi ha registres disponible en aquesta taula",
-    "info": "Mostrant del _START_ al _END_ d'un total de _TOTAL_ registres",
-    "infoEmpty": "No hi ha registres disponibles",
-    "infoFiltered": "(filtrat de _MAX_ registres)",
-    "thousands": ".",
-    "lengthMenu": "Mostra _MENU_ registres",
-    "loadingRecords": "Carregant...",
-    "processing": "Processant...",
-    "search": "Cerca:",
-    "zeroRecords": "No s'han trobat registres",
-    "paginate": {
-      "first": "Primer",
-      "last": "Últim",
-      "next": "Següent",
-      "previous": "Anterior"
-    },
-    "aria": {
-      "sortAscending": ": Activa per ordenar la columna de manera ascendent",
-      "sortDescending": ": Activa per ordenar la columna de manera descendent"
-    }
+    "search": "Cerca:"
   },
   "da": {
-    "decimal": ".",
-    "emptyTable": "Ingen data tilgængelige i tabellen",
-    "info": "Viser _START_ til _END_ af _TOTAL_ linjer",
-    "infoEmpty": "Viser 0 til 0 af 0 linjer",
-    "infoFiltered": "(filtreret fra _MAX_ linjer)",
-    "thousands": ".",
-    "lengthMenu": "Vis _MENU_ linjer",
-    "loadingRecords": "Henter...",
-    "processing": "Henter...",
-    "search": "Søg:",
-    "zeroRecords": "Ingen linjer matcher s&oslash;gningen",
-    "paginate": {
-      "first": "Første",
-      "last": "Sidste",
-      "next": "Næste",
-      "previous": "Forrige"
-    },
-    "aria": {
-      "sortAscending": "Stigende sortering",
-      "sortDescending": "Faldende sortering"
-    }
+    "search": "Søg:"
   },
   "de": {
-    "decimal": ",",
-    "emptyTable": "Keine Daten in der Tabelle vorhanden",
-    "info": "_START_ bis _END_ von _TOTAL_ Einträgen",
-    "infoEmpty": "Keine Daten vorhanden",
-    "infoFiltered": "(gefiltert von _MAX_ Einträgen)",
-    "thousands": ".",
-    "lengthMenu": "_MENU_ Zeilen anzeigen",
-    "loadingRecords": "Wird geladen ..",
-    "processing": "Bitte warten ..",
-    "search": "Suche:",
-    "zeroRecords": "Keine passenden Einträge gefunden",
-    "paginate": {
-      "first": "Erste",
-      "last": "Letzte",
-      "next": "Nächste",
-      "previous": "Zurück"
-    },
-    "aria": {
-      "sortAscending": ": aktivieren, um Spalte aufsteigend zu sortieren",
-      "sortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
-    }
+    "search": "Suche:"
   },
   "el": {
-    "decimal": ",",
-    "emptyTable": "Δεν υπάρχουν δεδομένα στον πίνακα",
-    "info": "Εμφανίζονται _START_ έως _END_ από _TOTAL_ εγγραφές",
-    "infoEmpty": "Εμφανίζονται 0 έως 0 από 0 εγγραφές",
-    "infoFiltered": "(φιλτραρισμένες από _MAX_ συνολικά εγγραφές)",
-    "thousands": ".",
-    "lengthMenu": "Δείξε _MENU_ εγγραφές",
-    "loadingRecords": "Φόρτωση...",
-    "processing": "Επεξεργασία...",
-    "search": "Αναζήτηση:",
-    "zeroRecords": "Δεν βρέθηκαν εγγραφές που να ταιριάζουν",
-    "paginate": {
-      "first": "Πρώτη",
-      "last": "Τελευταία",
-      "next": "Επόμενη",
-      "previous": "Προηγούμενη"
-    },
-    "aria": {
-      "sortAscending": ": ενεργοποιήστε για αύξουσα ταξινόμηση της στήλης",
-      "sortDescending": ": ενεργοποιήστε για φθίνουσα ταξινόμηση της στήλης"
-    }
+    "search": "Αναζήτηση:"
   },
   "et": {
-    "decimal": ".",
-    "emptyTable": "Andmed puuduvad",
-    "info": "Kuvatud: _TOTAL_ kirjet (_START_-_END_)",
-    "infoEmpty": "Otsinguvasteid ei leitud",
-    "infoFiltered": "(filteeritud _MAX_ kirje seast)",
-    "thousands": ",",
-    "lengthMenu": "N&auml;ita kirjeid _MENU_ kaupa",
-    "loadingRecords": "Laen...",
-    "processing": "Palun oodake, koostan kuvamiseks nimekirja!",
-    "search": "Otsi k&otilde;ikide tulemuste seast:",
-    "zeroRecords": "Otsitavat vastet ei leitud",
-    "paginate": {
-      "first": "Algus",
-      "last": "Viimane",
-      "previous": "Eelmine",
-      "next": "J&auml;rgmine"
-    },
-    "aria": {
-      "sortAscending": ": sorteeri kasvavalt",
-      "sortDescending": ": sorteeri kahanevalt"
-    }
+    "search": "Otsi k&otilde;ikide tulemuste seast:"
   },
   "fr": {
-    "decimal": ",",
-    "emptyTable": "Aucune donnée disponible dans le tableau",
-    "info": "Affichage de _START_ à _END_ sur _TOTAL_ éléments",
-    "infoEmpty": "Affichage de 0 à 0 sur 0 éléments",
-    "infoFiltered": "(filtrés depuis un total de _MAX_ éléments)",
-    "thousands": ".",
-    "lengthMenu": "Afficher _MENU_ éléments",
-    "loadingRecords": "Chargement...",
-    "processing": "Traitement...",
-    "search": "Rechercher:",
-    "zeroRecords": "Aucun élément correspondant trouvé",
-    "paginate": {
-      "first": "Premier",
-      "last": "Dernier",
-      "previous": "Précédent",
-      "next": "Suivant"
-    },
-    "aria": {
-      "sortAscending": ": activer pour trier la colonne par ordre croissant",
-      "sortDescending": ": activer pour trier la colonne par ordre décroissant"
-    }
+    "search": "Rechercher:"
   }
 }
