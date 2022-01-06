fa793076328403507ffba56964ced00366eef704
GordonDunsire <RSCChair@rdatoolkit.org>
2022-01-06 11:06:16 +0000

    Update testscript2.js

--- scripts/testscript2.js
+++ scripts/testscript2.js
@@ -122,7 +122,7 @@ function getLocalisation() {
   // Define a callback function
   xhttp.onload = function () {
     // Here you can use the Data
-//    localData = JSON.parse(this);
+    localData = JSON.parse(this.responseText);
     if (typeof this[window.theCurrentLanguageCode] != "undefined") {
       window.theLocalisationObject = this[window.theCurrentLanguageCode];
     }
