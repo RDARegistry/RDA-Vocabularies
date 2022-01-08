163853d6f35e02aba44ebfa6c5730de44a320a08
GordonDunsire <RSCChair@rdatoolkit.org>
2022-01-08 16:45:25 +0000

    Update testscript2.js

--- scripts/testscript2.js
+++ scripts/testscript2.js
@@ -586,7 +586,10 @@ function setDTDom() {
     //
     // Set the DOM to display all utilities
     //
-    window.domSetting = "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+    /*     window.domSetting = "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+    "<'row'<'col-sm-12'tr>>" +
+    "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"; */
+    window.domSetting = "<'row'<'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'f>>" +
     "<'row'<'col-sm-12'tr>>" +
     "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>";
   }
